e01314fb04222d896442a1524e09db2dd0a2737c
Hardik Vipulwar <hardikvipulwar123@gmail.com>
2020-06-04 09:52:06 +0530

    commits

---
+++ UI/hardik_project/src/App.js
@@ -0,0 +1,43 @@
+
+
+import React, { Component } from 'react';
+
+class App extends React.Component {
+
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         data: []
+      };
+   }
+
+//using api
+   async mymethod() {
+      // GET request using fetch with async/await
+      const response = await fetch('http://www.mocky.io/v2/5ecb5c353000008f00ddd5a0');
+      const data = await response.json();
+      this.setState({ data: data })
+      console.log(this.state);
+   }
+
+   render() {
+      return (
+         <div className="gallery">
+            {this.state.data.map((item) => <Image data={item.urls} />)}
+         </div>
+      );
+   }
+}
+
+//created a class serv
+class serv extends React.mymethod {
+   render() {
+      return (
+         <img key={this.props.data.full} src={this.props.data.full} srcSet={`${this.props.data.small} 300w, ${this.props.data.full} 768w, ${this.props.data.regular} 1280w, ${this.props.data.thumb} 3200w`}
+            alt="flex-gallery" className="flex-gallery" />
+      );
+   }
+}
+
+export default App;
